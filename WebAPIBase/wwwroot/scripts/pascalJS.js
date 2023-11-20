b30d650bbd060ff963cc9ce48f7c925cec2fae8f
Bence Tölgyszéki-Papp <bence.tolgyszeki-papp@stud.uni-corvinus.hu>
2023-11-20 15:10:12 +0100

    pascal hsz

---
+++ WebAPIBase/wwwroot/scripts/pascalJS.js
@@ -0,0 +1,31 @@
+﻿window.onload = function () {
+    initPascal(10);
+}
+
+var initPascal = function (n) {
+
+    var pascalDiv = document.getElementById('pascal');
+
+    for (var sor = 0; sor < n; sor++) {
+        var ujsorDiv = document.createElement('div');
+        ujsorDiv.classList.add('sor');
+        pascalDiv.appendChild(ujsorDiv);
+
+        for (var oszlop = 0;  oszlop <= sor;  oszlop++) {
+            var ujelemdiv = document.createElement('div');
+            ujelemdiv.classList.add('elem');
+            ujelemdiv.innerText = faktorialis(sor)/(faktorialis(oszlop)*faktorialis(sor-oszlop));
+            ujsorDiv.appendChild(ujelemdiv);
+        }
+    }
+}
+
+var faktorialis = function (n) {
+    er = 1;
+
+    for (var i = 1; i <= n; i++) {
+        er *= i;
+    }
+
+    return er;
+}
